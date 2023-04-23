79cad2909bab8f761783466195c671a1e306377a
MasonSeward1 <mason.seward49@ed.cna.nl.ca>
2023-04-23 10:12:19 -0230

    Modified navbar to contain links to all pages for demo. Added content to home page

--- react-front-end/src/Slideshow.js
+++ react-front-end/src/Slideshow.js
@@ -3,21 +3,11 @@ import Carousel from 'react-bootstrap/Carousel';
 export default function Homepage() {
     return (
         <>
-            <h1 id="data_display">Let's meet the team!</h1>
+            <h3 style={{textAlign: "center"}} >Taylors Insurance Demo</h3>
+            <img id="img" src="/logo.png" alt="company logo" style={{marginLeft: "44%", marginRight: "50%"}}/>
+            <p style={{textAlign: "center"}}>Welcome to the product demonstration of the insurance API for taylors insurance.</p><br/>
+            <h1 style={{textAlign: "center"}}>Let's meet the team!</h1>
             <Carousel variant="dark">
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="/logo.png"
-                        alt="First slide"
-                        style={{margin: "auto"}}
-                    />
-                    <Carousel.Caption style={{color: 'yellow'}}>
-                        <h3>Taylors Insurance Demo</h3>
-                        <p>Welcome to the product demonstration of the insurance API for taylors insurance.</p>
-                        <p>Meet the team!</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
@@ -53,7 +43,7 @@ export default function Homepage() {
                         style={{margin: "auto"}}
                     />
 
-                    <Carousel.Caption>
+                    <Carousel.Caption style={{color: 'red'}}>
                         <h3>Daniel Lewis</h3>
                         <p>Frontend Developer.</p>
                     </Carousel.Caption>
