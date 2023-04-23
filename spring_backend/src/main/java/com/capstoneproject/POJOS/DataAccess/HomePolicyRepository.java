55f612c01cb0d798608f2d3cc8af1157c7ae3af1
Julia Parewick <Julia.Parewick37@ed.cna.nl.ca>
2023-04-23 19:21:37 -0230

    same fix but needed to add policy deletions

--- spring_backend/src/main/java/com/capstoneproject/POJOS/DataAccess/HomePolicyRepository.java
+++ spring_backend/src/main/java/com/capstoneproject/POJOS/DataAccess/HomePolicyRepository.java
@@ -2,6 +2,10 @@ package com.capstoneproject.POJOS.DataAccess;
 
 import com.capstoneproject.HomePolicy;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
 public interface HomePolicyRepository extends CrudRepository<HomePolicy, Integer>{
 
+    Optional<HomePolicy> getByHomeId(Integer id);
 }
